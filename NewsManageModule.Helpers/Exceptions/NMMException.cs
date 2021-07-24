2fcd4974ec5bc51e56335d268d8b1f47c2df711c
itKhaiNDdotDev <khai12345678t@gmail.com>
2021-07-24 15:21:48 +0700

    Update Code: ...Helpers/Exceptions - add custom exceptions for services layers

---
+++ NewsManageModule.Helpers/Exceptions/NMMException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace NewsManageModule.Helpers.Exceptions
+{
+    public class NMMException : Exception
+    {
+        public NMMException()
+        {
+        }
+
+        public NMMException(string message) : base(message)
+        {
+        }
+
+        public NMMException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        //protected NMMException(SerializationInfo info, StreamingContext context) : base(info, context)
+        //{
+        //}
+    }
+}
